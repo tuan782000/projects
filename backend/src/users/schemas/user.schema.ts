b8628551541c282b8b16bcb21253ec1d9d8d3982
“tuan7820” <thaituan7820@gmail.com>
2024-12-11 18:55:00 +0700

    cập nhật cho user

--- backend/src/users/schemas/user.schema.ts
+++ backend/src/users/schemas/user.schema.ts
@@ -3,13 +3,40 @@ import { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema()
+@Schema({ timestamps: true })
 export class User {
     @Prop()
     name: string;
 
     @Prop()
-    age: number;
+    email: string;
+
+    @Prop()
+    password: string;
+
+    @Prop()
+    phone: string;
+
+    @Prop()
+    address: string;
+
+    @Prop()
+    image: string;
+
+    @Prop()
+    role: string;
+
+    @Prop()
+    accountType: string;
+
+    @Prop()
+    isActive: string;
+
+    @Prop()
+    codeId: string;
+
+    @Prop()
+    codeExpired: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
